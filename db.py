eccfca5b8a4f60496d563e95b9e97ae5989e383a
Nickosn <142935005+NicksonMakama@users.noreply.github.com>
2023-12-06 22:43:12 +0000

    workingDB

--- db.py
+++ db.py
@@ -17,16 +17,16 @@ except Exception as e:
     print(e)
 
 
-stopandemic_collection = client.stopandemic
+stopandemic_DB = client.stopandemic
 
 def setup_database():
-    shopping_list_db.drop_collection(shopping_list_db.items_collection)
-    items_collection = shopping_list_db.items_collection
-    for item in ['apples', 'broccoli', 'pizza', 'tangerine', 'potatoes']:
-        items_collection.insert_one({"description":item})
+    stopandemic_DB.drop_collection(stopandemic_DB.doctor)
+    items_collection = stopandemic_DB.doctor
+    for item in ['Nick', 'Paul', 'Rose']:
+        items_collection.insert_one({"Name":item})
 
 def get_items(id=None):
-    items_collection = shopping_list_db.items_collection
+    items_collection = stopandemic_DB.doctor
     if id == None:
         items = items_collection.find({})
     else:
@@ -37,27 +37,27 @@ def get_items(id=None):
     return items
 
 
-def add_item(description):
-    items_collection = shopping_list_db.items_collection
-    items_collection.insert_one({"description":description})
+def add_item(name):
+    items_collection = stopandemic_DB.doctor
+    items_collection.insert_one({"Name":name})
 
 def delete_item(id):
-    items_collection = shopping_list_db.items_collection
+    items_collection = stopandemic_DB.doctor
     items_collection.delete_one({"_id":ObjectId(id)})
 
-def update_item(id, description):
-    items_collection = shopping_list_db.items_collection
+def update_item(id, name):
+    items_collection = stopandemic_DB.doctor
     where = {"_id": ObjectId(id)}
-    updates = { "$set": { "description": description } }
+    updates = { "$set": { "Name": name } }
     items_collection.update_one(where, updates)
 
 def test_setup_database():
     print("testing setup_database()")
     setup_database()
     items = get_items()
-    assert len(items) == 5
-    descriptions = [item['description'] for item in items]
-    for description in ['apples', 'broccoli', 'pizza', 'tangerine', 'potatoes']:
+    assert len(items) == 3
+    descriptions = [item['Name'] for item in items]
+    for description in ['Nick', 'Paul', 'Rose']:
         assert description in descriptions
 
 def test_get_items():
@@ -69,14 +69,14 @@ def test_get_items():
     for item in items:
         assert 'id' in item
         assert type(item['id']) is str
-        assert 'description' in item
-        assert type(item['description']) is str
+        assert 'Name' in item
+        assert type(item['Name']) is str
     example_id = items[0]['id']
-    example_description = items[0]['description']
+    example_description = items[0]['Name']
     items = get_items(example_id)
     assert len(items) == 1
     assert example_id == items[0]['id']
-    assert example_description == items[0]['description']
+    assert example_description == items[0]['Name']
 
 def test_add_item():
     print("testing add_item()")
@@ -86,7 +86,7 @@ def test_add_item():
     add_item("licorice")
     items = get_items()
     assert len(items) == original_length + 1
-    descriptions = [item['description'] for item in items]
+    descriptions = [item['Name'] for item in items]
     assert "licorice" in descriptions
 
 
@@ -95,27 +95,27 @@ def test_delete_item():
     setup_database()
     items = get_items()
     original_length = len(items)
-    deleted_description = items[1]['description']
+    deleted_description = items[1]['Name']
     deleted_id = items[1]['id']
     delete_item(deleted_id)
     items = get_items()
     assert len(items) == original_length - 1
     for item in items:
         assert item['id'] != deleted_id
-        assert item['description'] != deleted_description
+        assert item['Name'] != deleted_description
 
 def test_update_item():
     print("testing update_item()")
     setup_database()
     items = get_items()
-    original_description = items[1]['description']
+    original_description = items[1]['Name']
     original_id = items[1]['id']
     update_item(original_id,"new-description")
     items = get_items()
     found = False
     for item in items:
         if item['id'] == original_id:
-            assert item['description'] == "new-description"
+            assert item['Name'] == "new-description"
             found = True
     assert found
 
